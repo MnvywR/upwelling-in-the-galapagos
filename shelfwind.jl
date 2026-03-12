70e1be90e50984ac861ae2040c2699c5cc1d6b54
Mason Redd <mwredd@ad.unc.edu>
2026-03-11 23:17:35 -0400

    Updated_Shelfwind_3112026
    Corrected: velocity, added kinetic energy (u,v,w), added vorticity
    
    Future plans: Correct initial conditions, correct Bathymetry model, better visualization (in separate code)

--- shelfwind.jl
+++ shelfwind.jl
@@ -1,3 +1,4 @@
+    using Revise
     using Pkg
     #Pkg.instantiate() # Only need to do this once when you started the repo in another machine
     #Pkg.resolve()
@@ -37,18 +38,18 @@
     ext_forcing = true
 
     if has_cuda_gpu()
-        arch = GPU()
-    else
+       # arch = GPU()
+    #else
         arch = CPU()
     end
     #---
 
     #It is in units of m
-    #Lx,y = 500-600 Km -> 500e3
+    #Lx,y = 500-600 Km -> 500e3 -> 1000 Km
     #Lz = 500 m 
     #++++ Construct grid
     if LES
-        params = (; Lx = 500e3,
+        params = (; Lx = 100e4,
                 Ly = 500e3,
                 Lz = 500,
                 Nx = 30,
@@ -83,30 +84,19 @@
 
     #----
 
-    #Changing Bathymetry to gaussian function for the island:
-    #height of 500 m (forgot what we said), 250km mean, 3e4 (30 km STD, width of island)
-    bottom(x,y) = -500 + 500 * exp( -(x-250e3)^2/(2*(3e4)^2) )* exp(-(y-0)^2/(2*(3e4)^2))
-
-    #Bottom bathymetry (with simple linear functions)
-    #bottom(x,y) =  max(- x*160/8000, -(x-8000)*40/42000-160)
+    #Bathymetry of Galapagos
+    #height of 500 m, 250km mean, 3e4 (30 km STD, w
+    bottom(x,y) = -500 + 500 * exp( -(x-params.Lx/2)^2/(2*(3e4)^2) )* exp(-(y-0)^2/(2*(3e4)^2))
 
     grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(bottom))
 
     @info "Grid" grid
-#=-
-    #++++ Creates a dictionary of simulation parameters
-    if mass_flux # m/s
-        u₁_west = .03 # mass inflow through west boundary
-    else
-        u₁_west = 0 # No mass flux
-    end
 
-=#
 
     # Not necessary, but makes organizing simulations easier and facilitates running on GPUs
     params = (; params...,
             N²₀ = 2e-4, #  9.83/1028*2/100  1/s (stratification frequency)
-            σ = 80000.0seconds, # s (relaxation timescale for sponge layer) how long we expect it to; now 1 day CHANGedto 40000 seconds (half a day)
+            σ = 40000.0seconds, # s (relaxation timescale for sponge layer) how long we expect it to; now 1 day CHANGedto 40000 seconds (half a day)
             #uₑᵥₐᵣ = 0.00, # m/s (velocity variation along the z direction of the east boundary)
             u_b = 0,    # m s⁻¹, average wind velocity 10 meters above the ocean
             v_b = 10,    #-10    # m s⁻¹, average wind velocity 10 meters above the ocean
@@ -117,7 +107,7 @@
     Umaxᵥ = 0.5 #m/s
     zₒᵥ = -75 #m 
     yₒᵥ = 0 #m
-    σ_zᵥ = 50 #m #change to around 20 ish
+    σ_zᵥ = 25 #m #change to around 20 ish
     σ_yᵥ = 55600 #0.5 * pi/180 * (6.371*10^6) #m = 55,600 meters #divide by two 
 
     U₁(y) = exp(-(y-yₒᵥ)^2/(2*σ_yᵥ^2))
@@ -211,18 +201,6 @@
         drag_bc_v = FluxBoundaryCondition(drag_v, field_dependencies=(:v, :v), parameters=(; cᴰ=cᴰ,))
     end
     #----
-    #----
-
-    #++++ Eastern BCs (freshwater flux)
-    if mass_flux # What comes in has to go out
-        params = (; params..., u_out = mean(u_west.(0, grid.zᵃᵃᶜ[1:Nz], 0, (params,))))
-        # The function below allows a net mass flux out of exactly u_out, but with variations in the form of
-        # a sine function. After a mean in z, this function return exactly u_out.
-        u∞(y, z, t, p) = p.u_out #+ p.uₑᵥₐᵣ*sin(-2π*z/grid.Lz) 
-    else
-        params = (; params..., u_out = 0)
-    end
-    #----
 
     #++++ West sponge layer 
     # (smoothes out the mass flux and gets rid of some of the build up of buoyancy)
@@ -251,13 +229,6 @@
         end
     end
 
-    #=
-    if mass_flux
-        @inline sponge_u(x, y, z, t, u, p) = -west_mask(x, y, z) * p.σ * (u - u∞(y, z, t, p)) # Nudges u to u∞
-        @inline sponge_v(x, y, z, t, v, p) = -west_mask(x, y, z) * p.σ * v # nudges v to zero
-        @inline sponge_w(x, y, z, t, w, p) = -west_mask(x, y, z) * p.σ * w # nudges w to zero
-    end
-    =#
     #@inline sponge_T(x, y, z, t, T, p) = -east_mask(x, y, z) / p.σ * (T - Teast(z, p))-west_mask(x, y, z) / p.σ * (T - (Twest(z, p))) # nudges T to T∞
     
     #Sponge layeres for the temperature, salinity, and eastward velocity 
@@ -268,28 +239,12 @@
     #----
 
     #++++ Assembling forcings and BCs
-    #if ext_forcing
-    # Fᵤ = Forcing(sponge_u, field_dependencies = :u, parameters = params)
-    # Fᵥ = Forcing(sponge_v, field_dependencies = :v, parameters = params)
-    # Fb = Forcing(sponge_b, field_dependencies = :b, parameters = params)
-    # forcing = (b=Fb,)
-    #else
-    #Added u = FU and forcing 
+ 
     FT = Forcing(sponge_T, field_dependencies = :T, parameters = params)
     FS = Forcing(sponge_S, field_dependencies = :S, parameters = params)
     FU = Forcing(sponge_u, field_dependencies = :u, parameters = params)
     forcing = (T=FT, S=FS, u = FU)
-    #end
-#=
-    if mass_flux
-        Fᵤ = Forcing(sponge_u, field_dependencies = :u, parameters = params)
-        Fᵥ = Forcing(sponge_v, field_dependencies = :v, parameters = params)
-        Fw = Forcing(sponge_w, field_dependencies = :w, parameters = params)
-        #wondering if I should change U V and W
-        #forcing = (T=FT, S=FS) so its only on T and S
-        forcing = (u=Fᵤ, v=Fᵥ, w=Fw, T=FT, S=FS)
-    end
-=#
+
     #Flux boundary conditions -> Value '''' and turned on west and east for Tbcs/Sbcs
     T_bcs = FieldBoundaryConditions(#top = ValueBoundaryCondition(get_T0, field_dependencies=(:T, :S)), 
                                     #west = ValueBoundaryCondition(20), #want to verify if it is Twest, etc. i.e. 20, 35
@@ -328,7 +283,7 @@
     #θ = 105 # degrees relative to pos. x-axis
 
 
-    phi = -0.7 * pi / 180 #this is in radians now, did -0.7 degrees for latitude 
+    phi = -0.0 * pi / 180 #this is in radians now, did -0.7 degrees for latitude 
     model = HydrostaticFreeSurfaceModel(grid, 
                                 #advection = WENO(grid=grid, order=5),
                                 #timestepper = :RungeKutta3, 
@@ -359,50 +314,20 @@
 
     Δt₀ = 1/2 * minimum_yspacing(grid) / 1 # / (u₁_west + 1)
     simulation = Simulation(model, Δt=Δt₀,
-                            stop_time = 15days, # when to stop the simulation
+                            stop_time = 100days, # when to stop the simulation
     )
 
-
-
-# --- CDF monitoring callback ---
-#=
-#--
-    function cdf_callback(sim)
-        T = sim.model.tracers.T
-        Tdata = isa(T.data, CuArray) ? Array(T.data) : T.data
-        vals = vec(Tdata)
-        nbins = 20
-        edges = range(minimum(vals), stop=maximum(vals), length=nbins+1)
-        hist = zeros(nbins)
-        for v in vals
-            idx = searchsortedlast(edges, v)
-            idx = clamp(idx, 1, nbins)
-            hist[idx] += 1
-        end
-        cdf = cumsum(hist) ./ length(vals)
-        @info "Step $(sim.iteration), time $(sim.time/3600) hrs:"
-        for i in 1:nbins
-            @printf(" %6.2f°C: %.2f |", edges[i], cdf[i])
-        end
-        println()
-    end
-=#
-    #simulation.callbacks[:cdf_monitor] = Callback(cdf_callback, IterationInterval(50))
+  
     #++++ Adapt time step
-    wizard = TimeStepWizard(cfl=0.5, # How to adjust the time step
+    wizard = TimeStepWizard(cfl=0.8, # How to adjust the time step
                         # diffusive_cfl=5,
                             max_change=1.02, min_change=0.2, min_Δt=0.1seconds) #max_Δt=0.5/√params.N²₀)
     simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(2)) # When to adjust the time step
     #----
 
     #++++ Printing to screen
+
     start_time = time_ns() * 1e-9
-
-    #hashing out since IDK the issue and it just does not exist???
-
-    #progress = SingleLineMessenger(SI_units=true,
-    #                                    initial_wall_time_seconds=start_time)
-    #simulation.callbacks[:progress] = Callback(progress, IterationInterval(5)) # when to print on screen
     callback_interval = 86400seconds
     progress = SingleLineMessenger()
     simulation.callbacks[:progress] = Callback(progress, TimeInterval(callback_interval))
@@ -482,27 +407,10 @@
     #++++ Outputs
     @info "Creating output fields"
 
-    #Added the below:
-    #NetCDFOutputWriter -> NetCDFWriter
-    #using Oceananigans.OutputWriters: NetCDFOutputWriter, Checkpointer
-
-
     # y-component of vorticity
-    ω_z = Field(∂x(v) - ∂y(u))
+    vorticity_z = Field(∂x(v) - ∂y(u))
 
-    #adding code for vorticity)
-    #simulation.output_writers[:vorticity] =
-    #NetCDFWriter(model, (; ω_z);
-    #    filename = "vorticity.nc",
-    #    schedule = TimeInterval(86400seconds)
-    #    )
     outputs = (; u, v, w, T,S,ω_z)
-    #write the above to the below 
-    simulation.output_writers[:surface_slice_writer] =
-        NetCDFWriter(model, (; u,v,w, T,S,ω_z); filename="vorticity.nc",
-                        schedule=TimeInterval(8640seconds), indices=(:, :, params.Nz),
-                            overwrite_existing = overwrite_existing)
-
 
     if mass_flux
         saved_output_prefix = "iceplume"
@@ -521,21 +429,18 @@
         overwrite_existing = true
     end
     #---
+#=
 
-    #Changed NetCdfOutputWriter -> NetCDFWriter in two below
-    #schedule=TimeInterval(8640) -> (8640seconds) 
-    #added a semicolon after model
-    #chaning indicies from (:,:,1) to what it is now
     simulation.output_writers[:surface_slice_writer] =
-        NetCDFWriter(model, (; u,v,w, T,S); filename="top.nc",
+        NetCDFWriter(model, (; u, v, w, T, S, vorticity_z); filename = joinpath(rundir, "top.nc"),
                         schedule=TimeInterval(8640seconds), indices=(:, :, params.Nz),
                             overwrite_existing = overwrite_existing)
     #same with below from indicies(:, round (params.NY/2),:)
     simulation.output_writers[:y_slice_writer] =
-        NetCDFWriter(model, (; u, v, w, T,S); filename="midy.nc",
+        NetCDFWriter(model, (; u, v, w, T, S, vorticity_z); filename= joinpath(rundir, "midy.nc"),
                         schedule=TimeInterval(8640seconds), indices=(:, Int(params.Ny/2), :), 
                         overwrite_existing = overwrite_existing)    
-
+=#
     ccc_scratch = Field{Center, Center, Center}(model.grid) # Create some scratch space to save memory
 
     uv = Field((@at (Center, Center, Center) u*v))
@@ -563,23 +468,12 @@
     vS_yavg = Average(vS, dims=(2))
     wS_yavg = Average(wS, dims=(2))
 
-#=
-    #NetCDFOutputWriter -> NetCDFWriter
-    #semicolon after (; u_yavg, etc..)
-    #I AM CHANING THIS TO 8640 SECONDS BECAUSE I DO NOT LIKE IT DAY BY DAY 
-    output_interval=8640seconds
-    simulation.output_writers[:averages] = NetCDFWriter(model, (; u_yavg, v_yavg, w_yavg, T_yavg, S_yavg, uv_yavg, uw_yavg, vw_yavg, uT_yavg, vT_yavg, wT_yavg,  uS_yavg, vS_yavg, wS_yavg, );
-                                                            schedule = AveragedTimeInterval(output_interval, window=output_interval),
-                                                            filename = "timeavgedfields_yavg.nc",
-                                                            overwrite_existing = overwrite_existing)
-
-=#
     KE = KineticEnergy(model)
-    ε = DissipationRate(model) #KEdissipationrate -> dissipationrate now
-    εᴰ = KineticEnergyStress(model) #changed from kineticenergydiffusiveterm -> KineticEnergyStress
+    ε = DissipationRate(model) 
+    εᴰ = KineticEnergyStress(model) 
     ∫KE = Integral(KE)
     ∫ε = Integral(ε)
-    ∫εᴰ = Integral(KineticEnergyStress(model)) #here too
+    ∫εᴰ = Integral(KineticEnergyStress(model)) 
 
     Q_inv = QVelocityGradientTensorInvariant(model)
     #KE_t = KineticEnergyTendency(model)
@@ -612,20 +506,30 @@
 
     KE_output_fields = (; KE_u, KE_v, KE_w, KE_total, KE_yavg, ε_yavg, ∫KE, ∫ε, ∫εᴰ, εᴰ_yavg, TKE_yavg, SP_x_yavg, SP_y_yavg, SP_z_yavg  )
 
-    #Changed output writer in two below to NetCDFWriter
-    #semicolon after kinetic fields 
-    #second -> seconds
-    #changed to 8640 from 86400 just to see :P
+    simulation.output_writers[:surface_slice_writer] =
+        NetCDFWriter(model, (; u, v, w, T, S, vorticity_z, KE_u, KE_v, KE_w, KE_total); filename = joinpath(rundir, "top.nc"),
+                        schedule=TimeInterval(8640seconds), indices=(:, :, params.Nz),
+                            overwrite_existing = overwrite_existing)
+    #same with below from indicies(:, round (params.NY/2),:)
+    simulation.output_writers[:y_slice_writer] =
+        NetCDFWriter(model, (; u, v, w, T, S, vorticity_z, KE_u, KE_v, KE_w, KE_total); filename= joinpath(rundir, "midy.nc"),
+                        schedule=TimeInterval(8640seconds), indices=(:, Int(params.Ny/2), :), 
+                        overwrite_existing = overwrite_existing)    
+
+    ccc_scratch = Field{Center, Center, Center}(model.grid) # Create some scratch space to save memory
+
+
     simulation.output_writers[:nc] = NetCDFWriter(model, KE_output_fields;
-                                                        filename = "KE_info.nc",
+                                                        filename = joinpath(rundir, "KE_info.nc"),
                                                         schedule = TimeInterval(8640seconds),
                                                         overwrite_existing = overwrite_existing)
+    #=                                                    
     simulation.output_writers[:nQ] = NetCDFWriter(model, (; Q=Q_inv);
-                                                        filename = "Q.nc",
+                                                        filename = joinpath(rundir, "Q.nc"),
                                                         schedule = TimeInterval(8640seconds),
                                                         overwrite_existing = overwrite_existing)
 
-
+    =#
     simulation.output_writers[:checkpointer] = Checkpointer(model,
                                                             schedule = TimeInterval(8640seconds),
                                                             prefix = checkpointer_prefix,
